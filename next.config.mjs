5b2cf31f3083bc57eb40e3c8f5b2c48c430d402c
Amarnath Devshatwar <amarnathdevshatwar.scoe.chem@gmail.com>
2024-10-08 15:22:03 +0530

    problems

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['flagcdn.com'], // if you are fetching external images from flagcdn.com
+    },
+    reactStrictMode: true,
+    swcMinify: true, // Optional performance improvement
+  };
+  
+  export default nextConfig;
+  
